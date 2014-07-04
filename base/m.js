5540714abfb383201d7cc496036b89854043845d
root <root@172-1-40-11.lightspeed.wepbfl.sbcglobal.net>
2014-07-04 23:58:30 +0800

    chang

--- base/m.js
+++ base/m.js
@@ -2,8 +2,9 @@ require.config({
 	baseUrl: 'http://fe.qyer.com/m',
 	paths:{
 		zepto:"base/js/zepto",
-		zepto-touch:"base/js/zepto-touch",
-		zepto-fx:"base/js/zepto-fx"
+		zepto_touch:"base/js/zepto-touch",
+		zepto_fx:"base/js/zepto-fx",
+		popup_base:"common/ui/popup_base/src/popup_base"
 	},
 	map: {
 		'*': {
